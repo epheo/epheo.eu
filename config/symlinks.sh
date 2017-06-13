c6dddd0692937dec4b95937f617d21f786235f2b
epheo <github@epheo.eu>
2017-06-13 16:36:24 +0000

    Update config sway and aliases and add identity picture

--- config/symlinks.sh
+++ config/symlinks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ln -s `pwd`/files/vimrc ~/.vimrc
-ln -s `pwd`/files/alias.sh ~/.alias.sh
+ln -s `pwd`/files/zsh_alias ~/.zsh_alias
 ln -s `pwd`/files/i3blocks.conf ~/.i3blocks.conf
 ln -s `pwd`/files/sway ~/.config/sway
+ln -s `pwd`/files/i3 ~/.config/i3
 ln -s `pwd`/files/Xdefaults ~/.Xdefaults
 ln -s `pwd`/files/zshrc ~/.zshrc
 ln -s `pwd`/files/vimperatorrc ~/.vimperatorrc
