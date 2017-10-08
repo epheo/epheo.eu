46ae739a31d7bce804a097592ee8176d8953e65b
Thibaut Lapierre <github@epheo.eu>
2017-10-08 18:29:56 +0200

    Work on make Arch cloudimg,
    Script is now working, images are created succesfuly.

--- autom/make_arch_cloudimg.sh
+++ autom/make_arch_cloudimg.sh
@@ -14,7 +14,7 @@ running_as_root() {
 K_MOD=`lsmod |grep loop`
 if [ -z "$K_MOD" ]; then sudo modprobe loop; fi;
 
-DATE=`date -iseconds |sed -r 's/[^a-za-z0-9]//g; s/0000/0/g'`
+DATE=`date -Iseconds |sed -r 's/[^a-za-z0-9]//g; s/0000/0/g'`
 FILE_NAME=arch-$DATE-x86_64
 AMI_NAME=${FILE_NAME}.raw
 QCOW2_NAME=${FILE_NAME}.qcow2
@@ -47,13 +47,13 @@ PARTED=/usr/bin/parted
 ${PARTED} -s ${AMI_NAME} mktable msdos
 ${PARTED} -s -a optimal ${AMI_NAME} mkpart primary ext4 1M 100%
 ${PARTED} -s ${AMI_NAME} set 1 boot on
-#install-mbr ${AMI_NAME}
 
 LOOP=`sudo kpartx -av ${AMI_NAME} |grep loop |sed -e "s/.*\(loop[^ ]*\).*/\1/"`
 
-sudo mkfs.ext4 /dev/mapper/${LOOP} || clean
+sudo mkfs.ext4 -O ^64bit /dev/mapper/${LOOP} || clean
+# -O ^64bit option as syslinux does not support it
 
-BLOCK_ID=`sudo blkid /dev/mapper/${LOOP} |cut -d ' ' -f2`
+BLOCK_ID=`sudo blkid /dev/mapper/${LOOP} |cut -d ' ' -f2 |tr -d \"`
 MOUNT_DIR=`mktemp -d -t build-img.XXXXXX`
 sudo mount -o loop /dev/mapper/${LOOP} ${MOUNT_DIR} || clean
 sudo pacstrap -c ${MOUNT_DIR} base || clean
@@ -66,15 +66,17 @@ cat <<__EOF__
 # ----------------------------
 __EOF__
 
+${CHROOT} pacman --noconfirm -S openssh cronie syslinux
+
 sudo arch-chroot ${MOUNT_DIR} sh -c "echo root:${R_PASSW} | chpasswd"
-sed -i "s/PermitRootLogin yes/PermitRootLogin without-password/" \
+sudo sed -i "s/PermitRootLogin yes/PermitRootLogin without-password/" \
   ${MOUNT_DIR}/etc/ssh/sshd_config
 
 echo '# Setup fstab'
 
 echo "# /etc/fstab: static file system information.
 proc	/proc	proc	nodev,noexec,nosuid	0	0
-UUID=${BLOCK_ID}	/	ext4	errors=remount-ro	0	1
+${BLOCK_ID}	/	ext4	errors=remount-ro	0	1
 " | sudo tee -a ${MOUNT_DIR}/etc/fstab
 
 echo '# Set cloud-arch as hostname'
@@ -85,39 +87,55 @@ ${CHROOT} rm /etc/localtime
 ${CHROOT} ln -s /usr/share/zoneinfo/UTC /etc/localtime
 
 echo '# Enable sshd, dhcpcd, cronie'
-${CHROOT} pacman --noconfirm -S openssh cronie syslinux
 ${CHROOT} systemctl enable dhcpcd@eth0.service
 ${CHROOT} systemctl enable cronie.service
 ${CHROOT} systemctl enable sshd.service
 sudo mkdir -p ${MOUNT_DIR}/root/.ssh
 
 echo '# Setting-up initramfs'
-${CHROOT} mkinitcpio -p linux
-sudo sed -i -r -e '/^HOOKS=/ { s/fsck/fsck growfs/ }' \
-  ${MOUNT_DIR}/etc/mkinitcpio.conf
-sudo sed -i -r -e '/^MODULES=/ { s/\"\"/\"virtio\ virtio_blk\ virtio_pci\ virtio_net\"/ }' \
+# Growfs used to autoresize image root disk to flavor root disk
+# mkinitcpio 
+
+#sudo sed -i -r -e '/^HOOKS=/ { s/fsck/fsck growfs/ }' \
+#  ${MOUNT_DIR}/etc/mkinitcpio.conf
+#sudo sed -i -r -e \
+#  '/^MODULES=/ { s/\"\"/\"virtio\ virtio_blk\ virtio_pci\ virtio_net\"/ }' \
+#  ${MOUNT_DIR}/etc/mkinitcpio.conf
+
+sudo sed -i \
+  '/^HOOKS=/c\HOOKS=\"base\ udev\ block\ autodetect\ modconf\ filesystems\ keyboard\ fsck\"' \
   ${MOUNT_DIR}/etc/mkinitcpio.conf
 
+sudo sed -i \
+  '/^MODULES=/c\MODULES=\"virtio\ virtio_blk\ virtio_pci\ virtio_net\"' \
+  ${MOUNT_DIR}/etc/mkinitcpio.conf
+
+cat ${MOUNT_DIR}/etc/mkinitcpio.conf |grep HOOKS
+cat ${MOUNT_DIR}/etc/mkinitcpio.conf |grep MODULES
+
+${CHROOT} mkinitcpio -p linux
 
 cat <<__EOF__
 # Setting-up syslinux
 # -------------------
 __EOF__
 
-sudo mkdir -p ${MOUNT_DIR}/boot/syslinux
-sudo cp ${MOUNT_DIR}/usr/lib/syslinux/bios/*.c32 ${MOUNT_DIR}/boot/syslinux/
+${CHROOT} mkdir -p /boot/syslinux
+${CHROOT} cp /usr/lib/syslinux/bios/*.c32 /boot/syslinux/
 KERNEL=`${CHROOT} find boot -name 'vmlinuz-linux'`
 RAMDISK=`${CHROOT} find boot -name 'initramfs-linux.img'`
-echo "default linux
-timeout 1
-label linux
-kernel ${KERNEL}
-append initrd=${RAMDISK} root=UUID=${BLOCK_ID} ro quiet" \
+echo "PROMPT 1
+TIMEOUT 50
+DEFAULT arch
+LABEL arch
+    LINUX /${KERNEL}
+    APPEND root=${BLOCK_ID} rw
+    INITRD /${RAMDISK}" \
   |sudo tee ${MOUNT_DIR}/boot/syslinux/syslinux.cfg
-${CHROOT} extlinux -i /boot/syslinux
-${CHROOT} dd bs=440 count=1 conv=notrunc \
-             if=/usr/lib/syslinux/bios/mbr.bin \
-             of=/dev/mapper/${LOOP}
+${CHROOT} extlinux --install /boot/syslinux/
+sudo dd bs=440 count=1 conv=notrunc \
+        if=${MOUNT_DIR}/usr/lib/syslinux/bios/mbr.bin \
+        of=${AMI_NAME}
 
 clean
 
@@ -134,4 +152,4 @@ cat <<__EOF__
 # ---------------------------------------
 # The created Arch guest system image is:
 __EOF__
-echo `pwd`$QCOW2_NAME
+echo `pwd`/$QCOW2_NAME
