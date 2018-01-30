56830e8ff30c50440d043ee840cec81ebc22f35a
epheo <github@epheo.eu>
2018-01-30 22:43:34 +0000

    Config, add screenrc

--- config/symlinks-server.sh
+++ config/symlinks-server.sh
@@ -3,3 +3,4 @@
 ln -s `pwd`/files/vimrc ~/.vimrc
 ln -s `pwd`/files/zsh_alias ~/.zsh_alias
 ln -s `pwd`/files/zshrc ~/.zshrc
+sudo ln -s `pwd`/files/screenrc /etc/screenrc
--- config/symlinks-workstation.sh
+++ config/symlinks-workstation.sh
@@ -7,3 +7,4 @@ ln -s `pwd`/files/Xdefaults ~/.Xdefaults
 ln -s `pwd`/files/vimperatorrc ~/.vimperatorrc
 ln -s `pwd`/files/zsh_alias ~/.zsh_alias
 ln -s `pwd`/files/zshrc ~/.zshrc
+sudo ln -s `pwd`/files/screenrc /etc/screenrc
